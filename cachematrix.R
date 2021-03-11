e28a24e7f91ad910927544f6274bc2dcec854757
ProttoyKumar <prottoykumar186@gmail.com>
2021-03-11 17:58:28 +0600

    getting ready to push an example cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#Closures get their name because they enclose the environment of the parent function and can access all its variables.
+#Closures are useful for making function factories, and are one way to manage mutable state in R.
 
+#It allows us to have two levels of parameters: a parent level that controls operation and a child level that does the work. 
+#makeCacheMatrix(), uses this idea to store x and inv in the enclosing environment of the set, get, setInverse, getInverse functions. That means the environment within which they were defined, i.e., the environment created by the makeCacheMatrix().
+
+#makeCacheMatrix(): This function creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+#cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+  
 }
